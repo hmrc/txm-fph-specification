cedfc4e784e5b900c557f8236f26b1f99165d930
Jakub Czeczótka <3732434+czeczotka@users.noreply.github.com>
2022-05-18 11:49:21 +0100

    CMQ-2062: Play security upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import sbt._
 object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.12.0"
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.24.0"
   )
 
   val test: Seq[ModuleID] = Seq(
